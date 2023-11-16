14f97eaa59bb4873d7466030ed7ff45d9fc9b68a
inercaso <inercaso@gmail.com>
2023-11-16 12:22:41 +0200

    added CoffeeType and Intensity enums for dynamic coffee selection.

---
+++ src/task_4/crema_and_creatives/CoffeeType.java
@@ -0,0 +1,8 @@
+package task_4.crema_and_creatives;
+
+public enum CoffeeType {
+    CAPPUCCINO,
+    PUMPKIN_SPICE_LATTE,
+    AMERICANO,
+    SYRUP_CAPPUCCINO
+}
---
+++ src/task_4/crema_and_creatives/Intensity.java
@@ -0,0 +1,5 @@
+package task_4.crema_and_creatives;
+
+public enum Intensity {
+    LIGHT, NORMAL, STRONG;
+}
