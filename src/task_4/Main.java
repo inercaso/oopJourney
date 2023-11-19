352980cf7218da2361b5b0fcd7f0d6c5d115e0d3
inercaso <inercaso@gmail.com>
2023-11-19 23:22:57 +0200

    modified main.java in task_4, so that it is separate from the coffeeshop package. migrated stuff into barista.java

---
+++ src/task_4/Main.java
@@ -0,0 +1,8 @@
+import task_4.crema_and_creatives.Barista;
+
+public class Main {
+    public static void main(String[] args) {
+        Barista barista = new Barista();
+        barista.run();
+    }
+}
