18ea9532a23bb2db0d374f2bb9889c9d9e8d1ef3
inercaso <inercaso@gmail.com>
2024-10-22 23:36:03 +0300

    modified individualcontainer class in order to be able to print individuals with odd and even ids, kind of a practice.

--- src/IndividualContainer.java
+++ src/IndividualContainer.java
@@ -5,7 +5,7 @@ public class IndividualContainer {
     // created the container itself for storing individuals
     private List<Individual> individuals;
 
-    // constr
+    // constructor
     public IndividualContainer() {
         this.individuals = new ArrayList<>();
     }
@@ -24,6 +24,34 @@ public class IndividualContainer {
     public void printAll() {
         for (Individual individual : individuals) {
             System.out.println(individual);
+        }
+    }
+
+    // printing only the ids
+    public void printIds() {
+        System.out.println("Individual IDs:");
+        for (Individual individual : individuals) {
+            System.out.println("ID: " + individual.getId());
+        }
+    }
+
+    // filtering individuals with even ids
+    public void printEvenIds() {
+        System.out.println("Individuals with Even IDs:");
+        for (Individual individual : individuals) {
+            if (individual.getId() % 2 == 0) {
+                System.out.println(individual);
+            }
+        }
+    }
+
+    // filtering individuals with odd ids
+    public void printOddIds() {
+        System.out.println("Individuals with Odd IDs:");
+        for (Individual individual : individuals) {
+            if (individual.getId() % 2 != 0) {
+                System.out.println(individual);
+            }
         }
     }
 }
