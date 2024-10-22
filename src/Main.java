f1ef8427c0817d767d1dee036ed5728fb9cd0943
inercaso <inercaso@gmail.com>
2024-10-22 22:55:18 +0300

    added json parsing to main class, implemented org.json lib.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        Individual individual = new Individual(1, true, "Earth", 451, new String[]{"BLONDE", "POINTY_EARS"});
+        try {
+            // reads the input.json file
+            String filePath = "src/input.json";
+            String fileContent = FileReaderUtil.readFile(filePath);
 
-        System.out.println(individual);
+            // parsing the content as a JSON object
+            JSONObject jsonObject = new JSONObject(fileContent);
+
+            JSONArray individualsArray = jsonObject.getJSONArray("data");
+
+            // prints each JSON object separately
+            for (int i = 0; i < individualsArray.length(); i++) {
+                JSONObject individual = individualsArray.getJSONObject(i);
+                System.out.println("Individual JSON Object " + (i + 1) + ": " + individual);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
     }
 }
