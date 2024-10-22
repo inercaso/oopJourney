f3f9cfb7c0ec2e80ab9098012126de9f99fd3193
inercaso <inercaso@gmail.com>
2024-10-23 01:17:40 +0300

    updated the main in order to write classified individuals to 4 total json files grouped by universe.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,8 +26,12 @@ public class Main {
                 // classifier
                 Classifier classifier = new Classifier();
 
+                // view to handle writing to files
+                View view = new View();
+
                 // map each JSON object to an Individual object and then add to container
-                for (int i = 0; i < individualsArray.length(); i++) {  // iterate through all individuals
+                Map<String, List<Individual>> classifiedIndividuals = new HashMap<>();  // to store individuals by universe
+                for (int i = 0; i < individualsArray.length(); i++) {
                     JSONObject individualJson = individualsArray.getJSONObject(i);
 
                     int id = individualJson.getInt("id");
@@ -47,8 +55,13 @@ public class Main {
 
                     // classify the individual
                     String classification = classifier.classify(individual);
-                    System.out.println("Individual ID: " + id + " classified as: " + classification);
+
+                    // add the individual to the corresponding universe list
+                    classifiedIndividuals.computeIfAbsent(classification, k -> new ArrayList<>()).add(individual);
                 }
+
+                // write the grouped individuals to JSON files using the View class
+                view.writeToJson(classifiedIndividuals);
 
             } else {
                 System.err.println("Error: 'data' key not found in JSON.");
