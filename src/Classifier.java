9066b8dbd06e88fe7c0c945c5ef0ead5ee099e58
inercaso <inercaso@gmail.com>
2024-10-27 12:10:12 +0200

    updated classifier constructor to accept universes list as parameter.

--- src/Classifier.java
+++ src/Classifier.java
@@ -7,30 +7,10 @@ public class Classifier {
     private List<Universe> universes;  // list to store all universes
     private Map<String, Integer> scores;  // map to store scores for each universe
 
-    // constructor used to initialize universes
-    public Classifier() {
-        universes = new ArrayList<>();
+    // constructor receives universes as parameter
+    public Classifier(List<Universe> universes) {
+        this.universes = universes;
         scores = new HashMap<>();
-
-        // adding universes to the list
-        universes.add(new Universe("Star Wars", "Kashyyyk", "Endor", new String[][] {
-                {"HAIRY", "TALL"},  // wookie
-                {"SHORT", "HAIRY"}  // ewok
-        }, 400, 60));
-
-        universes.add(new Universe("Marvel", "Asgard", null, new String[][] {
-                {"BLONDE", "TALL"}  // asgardian
-        }, 5000, null));
-
-        universes.add(new Universe("Hitchhiker's Guide", "Betelgeuse", "Vogsphere", new String[][] {
-                {"EXTRA_ARMS", "EXTRA_HEAD"},  // betelgeusian
-                {"GREEN", "BULKY"}  // vogon
-        }, 100, 200));
-
-        universes.add(new Universe("Lord of the Rings", "Earth", null, new String[][] {
-                {"BLONDE", "POINTY_EARS"},  // elf
-                {"SHORT", "BULKY"}  // warf
-        }, null, 200));
     }
 
     // classify an individual by calculating
@@ -40,19 +20,23 @@ public class Classifier {
         for (Universe universe : universes) {
             int score = universe.calculateScore(individual);
             scores.put(universe.getName(), score);
+            System.out.println("Score for " + universe.getName() + ": " + score);  // debug print
         }
 
         // find the universe with the highest score
-        return findMaxScore();
+        String bestMatch = findMaxScore();
+        System.out.println("Best match for individual " + individual.getId() + ": " + bestMatch);  // debug print
+
+        return bestMatch;
     }
 
     // helper method to find the universe with the highest score
     private String findMaxScore() {
         String maxUniverse = null;
-        int maxScore = -1;
+        int maxScore = 0;  // setting a minimum threshold of 0
 
         for (Map.Entry<String, Integer> entry : scores.entrySet()) {
-            if (entry.getValue() > maxScore) {
+            if (entry.getValue() > maxScore) {  // only consider positive scores
                 maxScore = entry.getValue();
                 maxUniverse = entry.getKey();
             }
